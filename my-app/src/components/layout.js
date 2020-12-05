a9dd3a33797232eb19b682bb481e71328a85bba1
isagnik <isagnikbhattacharjee@gmail.com>
2020-12-05 17:40:23 -0500

    Header and footed added

--- my-app/src/components/layout.js
+++ my-app/src/components/layout.js
@@ -33,39 +33,10 @@ const Layout = ({ children }) => {
         </header>
         <main>{children}</main>
         <footer>
-        <Navbar bg="light">
-                <Navbar.Brand href="#home">Brand link</Navbar.Brand>
-            </Navbar>
-            <br />
-            <Navbar bg="light">
+            <Navbar bg="dark">
                 <Navbar.Brand>Brand text</Navbar.Brand>
             </Navbar>
-            <br />
-            <Navbar bg="dark">
-                <Navbar.Brand href="#home">
-                <img
-                    src="/logo.svg"
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                    alt="React Bootstrap logo"
-                />
-                </Navbar.Brand>
-            </Navbar>
-            <br />
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">
-                <img
-                    alt=""
-                    src="/logo.svg"
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                />{' '}
-                React Bootstrap
-                </Navbar.Brand>
-            </Navbar>
-                    </footer>
+        </footer>
                 </>
     )
 }
