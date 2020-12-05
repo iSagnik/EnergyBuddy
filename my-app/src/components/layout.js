f6c285be59d874064a7741aa8d66b6eea5525bdf
Simran Sidhu <narmis99@gmail.com>
2020-12-05 17:45:21 -0500

    with header and footer

--- my-app/src/components/layout.js
+++ my-app/src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Navbar from 'react-bootstrap/Navbar'
+import {Nav, NavDropdown, Navbar} from 'react-bootstrap'
 
 const Layout = ({ children }) => {
 
