a544e64219e29a36a2c74ce776e21897c81d6c81
Arpita Das <arpitad@live.unc.edu>
2020-12-06 04:30:11 -0500

    community doesn't work yet

--- my-app/src/components/community.js
+++ my-app/src/components/community.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './searchbar.js';
 import axios from 'axios';
+import { Button } from 'react-bootstrap';
 
 export default function Community() {
+
+    const[allUsers, setUsers] = useState([]);
 
     
     async function getUsers() {
@@ -10,13 +13,24 @@ export default function Community() {
             method: 'get',
             url: `https://sustainability-goals-default-rtdb.firebaseio.com/users.json`,
             withCredientials: true
-           })
-           const response = await result
-           console.log(response);
+           }).then((x) => {
+               setUsers(Object.keys(x.data));
+           }).then(console.log(allUsers));
+    }
+
+    async function handleAddFriend() {
+        console.log("added friend");
     }
 
 
+    useEffect(() => {
+        getUsers();
+    }, [])
 
-    return ( <div></div>);
+    return ( 
+    <div className="container">
+        {allUsers && <SearchBar items={allUsers}/>}
+        <Button onClick = { handleAddFriend } variant="primary">  Add Friend </Button>
+    </div>);
     
 }
