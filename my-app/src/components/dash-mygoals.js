299ae4345113f6d29c7202b142fa40cec880af68
isagnik <isagnikbhattacharjee@gmail.com>
2020-12-06 12:31:42 -0500

    Goals list works except actually displaying it lol

--- my-app/src/components/dash-mygoals.js
+++ my-app/src/components/dash-mygoals.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Col, Row, ListGroup, Button } from 'react-bootstrap'
-import axios from 'axios';
+import { Col, Row, ListGroup } from 'react-bootstrap'
 
 const MyGoalsDash = ( {goals} ) => {
     
