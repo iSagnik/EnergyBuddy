6e906aeb64615864386049f2675a9f01604080b8
Arpita Das <arpitad@live.unc.edu>
2020-12-06 03:05:17 -0500

    signup -> login app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,7 +14,7 @@ function App() {
             {/* <Route path="/signup" component={SignUp} /> */}
             <Route path="/goals" component={Goals} />
             <Route path="/dashboard" component={DashBoard}/>
-            <Route exact path="/" component={SignUp}/>
+            <Route exact path="/" component={Login}/>
             
         </Switch> 
       </AuthProvider>
